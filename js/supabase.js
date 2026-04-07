662ea7c1d099c546b97805a96fe3dfdb931205f1
JansenTiel <cmjinhuur@gmail.com>
2026-04-07 11:25:57 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,16 @@
+window.SUPABASE_URL = 'https://nfkyezemyedvfyqimhpm.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_bD0410b0UhusLqrMvKcoEw_2FCi9duM';
 
+window.sb = function(){
+  if(!window.supabase || !window.supabase.createClient)
+    throw new Error('Supabase library niet geladen.');
+
+  if(!window._sbClient){
+    window._sbClient = window.supabase.createClient(
+      window.SUPABASE_URL,
+      window.SUPABASE_ANON_KEY
+    );
+  }
+
+  return window._sbClient;
+};
